c785f333ca7ca512a5859938e6b36a67e57cffde
rajat7014 <132184024+rajat7014@users.noreply.github.com>
2025-03-24 14:32:10 +0530

    initial commit

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -61,7 +61,7 @@ export default async function Page() {
   ]
 
   const todaysDeals = await getProductsByTag({ tag: 'todays-deal' })
-
+  const bestSellingProducts = await getProductsByTag({ tag: 'best-seller' })
   return (
     <>
       <HomeCarousel items={data.carousels}></HomeCarousel>
@@ -73,6 +73,16 @@ export default async function Page() {
             <ProductSlider
               title={"Today's Deals"}
               products={todaysDeals}
+            ></ProductSlider>
+          </CardContent>
+        </Card>
+
+        <Card className='w-full rounded-none'>
+          <CardContent className='p-4 items-center gap-3'>
+            <ProductSlider
+              title={'Best Sellling Products'}
+              products={bestSellingProducts}
+              hideDetails
             ></ProductSlider>
           </CardContent>
         </Card>
